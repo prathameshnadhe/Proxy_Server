bc2f11e9500e458cdd6146d727e526104bbb91f0
prathameshnadhe <prathameshnadhe06@gmail.com>
2024-08-29 22:20:58 +0530

    Add API for restaurant Search

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Proxy endpoint
+// Swiggy's Proxy endpoint for Restaurants
 app.get("/restaurants", (req, res) => {
   const apiUrl =
     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5162434&lng=73.8428574&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
@@ -44,6 +44,7 @@ app.get("/restaurants", (req, res) => {
   });
 });
 
+// Swiggy's Proxy endpoint to infinite scroll for Restaurants
 app.get("/restaurantsUpdate", async (req, res) => {
   const nextOffset = req.query.offset || "CJhlELQ4KICQ4IWS1ZmFNzCnEzgC";
   const collection = req.query.collection || "12";
@@ -64,14 +65,14 @@ app.get("/restaurantsUpdate", async (req, res) => {
       body: JSON.stringify({
         lat: 18.5162434,
         lng: 73.8428574,
-        nextOffset: nextOffset, // Pass the current offset
+        nextOffset: nextOffset,
         widgetOffset: {
           NewListingView_category_bar_chicletranking_TwoRows: "",
           NewListingView_category_bar_chicletranking_TwoRows_Rendition: "",
           Restaurant_Group_WebView_PB_Theme: "",
           Restaurant_Group_WebView_SEO_PB_Theme: "",
           collectionV5RestaurantListWidget_SimRestoRelevance_food_seo:
-            collection, // Use dynamic collection value
+            collection,
           inlineFacetFilter: "",
           restaurantCountWidget: "",
         },
@@ -100,11 +101,11 @@ app.get("/restaurantsUpdate", async (req, res) => {
   }
 });
 
-// Endpoint to fetch the menu for a specific restaurant
+// Swiggy's Proxy endpoint to fetch the menu for a specific Restaurant
 app.get("/restaurant/:restaurantId", async (req, res) => {
   const { restaurantId } = req.params;
-  const lat = "18.5162434"; // Replace with your latitude if needed
-  const lng = "73.8428574"; // Replace with your longitude if needed
+  const lat = "18.5162434";
+  const lng = "73.8428574";
 
   const swiggyUrl = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&restaurantId=${restaurantId}&catalog_qa=undefined&submitAction=ENTER`;
 
@@ -122,6 +123,37 @@ app.get("/restaurant/:restaurantId", async (req, res) => {
     const response = await axios.get(swiggyUrl, { headers: options.headers });
     res.send(response.data);
   } catch (error) {
+    res.status(500).send("Error occurred while fetching restaurant menu");
+  }
+});
+
+// Swiggy's Proxy endpoint to search the Restaurant/Menu
+app.get("/restaurantSearch", async (req, res) => {
+  const { str } = req.query;
+  const lat = "18.5162434";
+  const lng = "73.8428574";
+
+  const swiggyUrl = `https://www.swiggy.com/dapi/restaurants/search/suggest?lat=${lat}&lng=${lng}&str=${str}&trackingId=undefined&includeIMItem=true`;
+
+  const cookies =
+    "_SW=3ey9qDDNkbrYJ79el6TiWoMYJ5k5ldek; _device_id=2f6f2f0f-11c1-fe31-e71c-922060a09607; fontsLoaded=1; _ot=REGULAR; _is_logged_in=1; _session_tid=9d61182ecee2bcb8bc891d496429df38dd0f9c172710cdc577403731f60ec425e3bac0bf0ec065668cef45c32332e7efe96d8dd4ed4788719b15232ba1f1fcb750254c1d9178e73aaacdf48d3404c0287bb2df48cad22701abc37d92a3b9248232ced0d92c0e38a1c50a26245a964f2f; deliveryAddressId=138114441; userLocation=%7B%22lat%22%3A%2218.523835%22%2C%22lng%22%3A%2273.826698%22%2C%22address%22%3A%22Gokhalenagar%2C%20Pune%2C%20Maharashtra%2C%20India%22%2C%22area%22%3A%22Gokhalenagar%22%2C%22id%22%3A%22138114441%22%2C%22showUserDefaultAddressHint%22%3Afalse%7D; _sid=ft986b19-c7a3-4726-b577-4b3bf71f4c5b";
+
+  const options = {
+    url: swiggyUrl,
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+      Accept: "application/json",
+      Origin: "https://www.swiggy.com",
+      Cookie: cookies,
+    },
+  };
+
+  try {
+    const response = await axios.get(swiggyUrl, { headers: options.headers });
+    res.send(response.data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
     res.status(500).send("Error occurred while fetching restaurant menu");
   }
 });
